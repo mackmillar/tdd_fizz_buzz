e8dc68c27e08b2ecc894887dbd3bf84cce40688a
mack millar <mackmillar@gmail.com>
2020-11-26 11:59:35 +0000

    fifth commit

--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -6,6 +6,9 @@ class TestFizzBuzz(unittest.TestCase):
     def test_fizz_buzz__3_returns_fizz(self):
         self.assertEqual("fizz", fizz_buzz(3))
 
+    def test_fizz_buzz__6_returns_fizz(self):
+        self.assertEqual("fizz", fizz_buzz(6))
+
     def test_fizz_buzz__5_returns_buzz(self):
         self.assertEqual("buzz", fizz_buzz(5))
 
@@ -15,4 +18,4 @@ class TestFizzBuzz(unittest.TestCase):
     def test_fizz_buzz__4_returns_4_as_str(self):
         self.assertEqual("4", fizz_buzz(4))
 
-    
+  
